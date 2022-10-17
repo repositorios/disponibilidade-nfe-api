66b82e53a9b01befe7cc4bb78e02774e53bcdeab
Renato Tavares <dr.renatotavares@gmail.com>
2022-10-17 10:52:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,2 @@
 
-sudo apt install python3 &&
-sudo apt install python3-pip &&
-pip3 install requests &&
-pip3 install beautifulsoup4 &&
-
-sudo cp src/cron.py /var/www/html/
+sudo apt install python3 && sudo apt install python3-pip && pip3 install requests && pip3 install beautifulsoup4 && sudo cp src/cron.py /var/www/html/
